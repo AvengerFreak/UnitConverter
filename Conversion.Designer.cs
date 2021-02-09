93bb6b3dd6f5bb7001486a6adf20bbf8c7f3e505
AvengerFreak <Shaina.Munoz@upr.edu>
2021-02-09 12:11:22 -0400

    Initial commit
    Contains a C# unit converter that extracts conversion rates from a textfile

---
+++ Conversion.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Converter
+{
+    partial class Conversion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Temperature_Label = new System.Windows.Forms.Label();
+            this.Mass_Label = new System.Windows.Forms.Label();
+            this.Presure_Label = new System.Windows.Forms.Label();
+            this.Temperature_TextBox = new System.Windows.Forms.TextBox();
+            this.Mass_TextBox = new System.Windows.Forms.TextBox();
+            this.Pressure_TextBox = new System.Windows.Forms.TextBox();
+            this.Pressure_ComboBox = new System.Windows.Forms.ComboBox();
+            this.Mass_ComboBox = new System.Windows.Forms.ComboBox();
+            this.Temperature_ComboBox = new System.Windows.Forms.ComboBox();
+            this.Convert_Btn = new System.Windows.Forms.Button();
+            this.Temperature_Convertion_View = new System.Windows.Forms.TextBox();
+            this.Mass_Convertion_View = new System.Windows.Forms.TextBox();
+            this.Pressure_Convertion_View = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Temperature_Label
+            // 
+            this.Temperature_Label.AutoSize = true;
+            this.Temperature_Label.Location = new System.Drawing.Point(40, 26);
+            this.Temperature_Label.Name = "Temperature_Label";
+            this.Temperature_Label.Size = new System.Drawing.Size(90, 17);
+            this.Temperature_Label.TabIndex = 0;
+            this.Temperature_Label.Text = "Temperatura";
+            // 
+            // Mass_Label
+            // 
+            this.Mass_Label.AutoSize = true;
+            this.Mass_Label.Location = new System.Drawing.Point(363, 26);
+            this.Mass_Label.Name = "Mass_Label";
+            this.Mass_Label.Size = new System.Drawing.Size(42, 17);
+            this.Mass_Label.TabIndex = 1;
+            this.Mass_Label.Text = "Masa";
+            // 
+            // Presure_Label
+            // 
+            this.Presure_Label.AutoSize = true;
+            this.Presure_Label.Location = new System.Drawing.Point(575, 26);
+            this.Presure_Label.Name = "Presure_Label";
+            this.Presure_Label.Size = new System.Drawing.Size(56, 17);
+            this.Presure_Label.TabIndex = 2;
+            this.Presure_Label.Text = "Presión";
+            // 
+            // Temperature_TextBox
+            // 
+            this.Temperature_TextBox.Location = new System.Drawing.Point(12, 76);
+            this.Temperature_TextBox.Name = "Temperature_TextBox";
+            this.Temperature_TextBox.Size = new System.Drawing.Size(100, 22);
+            this.Temperature_TextBox.TabIndex = 3;
+            // 
+            // Mass_TextBox
+            // 
+            this.Mass_TextBox.Location = new System.Drawing.Point(286, 76);
+            this.Mass_TextBox.Name = "Mass_TextBox";
+            this.Mass_TextBox.Size = new System.Drawing.Size(100, 22);
+            this.Mass_TextBox.TabIndex = 4;
+            // 
+            // Pressure_TextBox
+            // 
+            this.Pressure_TextBox.Location = new System.Drawing.Point(551, 76);
+            this.Pressure_TextBox.Name = "Pressure_TextBox";
+            this.Pressure_TextBox.Size = new System.Drawing.Size(100, 22);
+            this.Pressure_TextBox.TabIndex = 5;
+            // 
+            // Pressure_ComboBox
+            // 
+            this.Pressure_ComboBox.FormattingEnabled = true;
+            this.Pressure_ComboBox.Items.AddRange(new object[] {
+            "kg/(cm^2)",
+            "PSI",
+            "KPa"});
+            this.Pressure_ComboBox.Location = new System.Drawing.Point(667, 74);
+            this.Pressure_ComboBox.Name = "Pressure_ComboBox";
+            this.Pressure_ComboBox.Size = new System.Drawing.Size(121, 24);
+            this.Pressure_ComboBox.TabIndex = 6;
+            // 
+            // Mass_ComboBox
+            // 
+            this.Mass_ComboBox.FormattingEnabled = true;
+            this.Mass_ComboBox.Items.AddRange(new object[] {
+            "cm^3",
+            "m^3",
+            "ft"});
+            this.Mass_ComboBox.Location = new System.Drawing.Point(392, 74);
+            this.Mass_ComboBox.Name = "Mass_ComboBox";
+            this.Mass_ComboBox.Size = new System.Drawing.Size(121, 24);
+            this.Mass_ComboBox.TabIndex = 7;
+            // 
+            // Temperature_ComboBox
+            // 
+            this.Temperature_ComboBox.FormattingEnabled = true;
+            this.Temperature_ComboBox.Items.AddRange(new object[] {
+            "Celsio",
+            "Farenhiet",
+            "Kelvin"});
+            this.Temperature_ComboBox.Location = new System.Drawing.Point(118, 74);
+            this.Temperature_ComboBox.Name = "Temperature_ComboBox";
+            this.Temperature_ComboBox.Size = new System.Drawing.Size(121, 24);
+            this.Temperature_ComboBox.TabIndex = 8;
+            // 
+            // Convert_Btn
+            // 
+            this.Convert_Btn.Location = new System.Drawing.Point(12, 396);
+            this.Convert_Btn.Name = "Convert_Btn";
+            this.Convert_Btn.Size = new System.Drawing.Size(126, 23);
+            this.Convert_Btn.TabIndex = 9;
+            this.Convert_Btn.Text = "Convertir";
+            this.Convert_Btn.UseVisualStyleBackColor = true;
+            this.Convert_Btn.Click += new System.EventHandler(this.Convert_Btn_Click);
+            // 
+            // Temperature_Convertion_View
+            // 
+            this.Temperature_Convertion_View.Enabled = false;
+            this.Temperature_Convertion_View.Location = new System.Drawing.Point(12, 143);
+            this.Temperature_Convertion_View.Multiline = true;
+            this.Temperature_Convertion_View.Name = "Temperature_Convertion_View";
+            this.Temperature_Convertion_View.Size = new System.Drawing.Size(227, 193);
+            this.Temperature_Convertion_View.TabIndex = 10;
+            // 
+            // Mass_Convertion_View
+            // 
+            this.Mass_Convertion_View.Enabled = false;
+            this.Mass_Convertion_View.Location = new System.Drawing.Point(286, 143);
+            this.Mass_Convertion_View.Multiline = true;
+            this.Mass_Convertion_View.Name = "Mass_Convertion_View";
+            this.Mass_Convertion_View.Size = new System.Drawing.Size(227, 193);
+            this.Mass_Convertion_View.TabIndex = 11;
+            // 
+            // Pressure_Convertion_View
+            // 
+            this.Pressure_Convertion_View.Enabled = false;
+            this.Pressure_Convertion_View.Location = new System.Drawing.Point(551, 143);
+            this.Pressure_Convertion_View.Multiline = true;
+            this.Pressure_Convertion_View.Name = "Pressure_Convertion_View";
+            this.Pressure_Convertion_View.Size = new System.Drawing.Size(237, 193);
+            this.Pressure_Convertion_View.TabIndex = 12;
+            // 
+            // Conversion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Pressure_Convertion_View);
+            this.Controls.Add(this.Mass_Convertion_View);
+            this.Controls.Add(this.Temperature_Convertion_View);
+            this.Controls.Add(this.Convert_Btn);
+            this.Controls.Add(this.Temperature_ComboBox);
+            this.Controls.Add(this.Mass_ComboBox);
+            this.Controls.Add(this.Pressure_ComboBox);
+            this.Controls.Add(this.Pressure_TextBox);
+            this.Controls.Add(this.Mass_TextBox);
+            this.Controls.Add(this.Temperature_TextBox);
+            this.Controls.Add(this.Presure_Label);
+            this.Controls.Add(this.Mass_Label);
+            this.Controls.Add(this.Temperature_Label);
+            this.Name = "Conversion";
+            this.Text = "Convertidor de Unidades";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Temperature_Label;
+        private System.Windows.Forms.Label Mass_Label;
+        private System.Windows.Forms.Label Presure_Label;
+        private System.Windows.Forms.TextBox Temperature_TextBox;
+        private System.Windows.Forms.TextBox Mass_TextBox;
+        private System.Windows.Forms.TextBox Pressure_TextBox;
+        private System.Windows.Forms.ComboBox Pressure_ComboBox;
+        private System.Windows.Forms.ComboBox Mass_ComboBox;
+        private System.Windows.Forms.ComboBox Temperature_ComboBox;
+        private System.Windows.Forms.Button Convert_Btn;
+        private System.Windows.Forms.TextBox Temperature_Convertion_View;
+        private System.Windows.Forms.TextBox Mass_Convertion_View;
+        private System.Windows.Forms.TextBox Pressure_Convertion_View;
+    }
+}
+
---
+++ Conversion.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Converter
+{
+    public partial class Conversion : Form
+    {
+        float kelvinConversionRate;
+        float celsisFarenhietConversionRate;
+
+        float cmConversionRateToMeter;
+        float cmConversionRateToFt;
+        float mConversionRateToCentimeter;
+        float mConversionRateToFt;
+        float ftConversionRateToCentimeter;
+        float ftConversionRateToMeter;
+
+        float kgPerCmSquaredConversionRateToPsi;
+        float kgPerCmSquaredConversionRateToKPa;
+        float psiConversionRateTokgPerCmSquared;
+        float psiConversionRateToKPa;
+        float kPaConversionRateToKgPerCmSquared;
+        float kPaConversionRateToPsi;
+        
+
+        public Conversion()
+        {
+            InitializeComponent();
+        }
+
+        private void convertTemperature() { 
+        
+            string input =  Temperature_TextBox.Text;
+
+            float initialValue = (float) Convert.ToDouble(input);
+
+            int selectedIndex = Temperature_ComboBox.SelectedIndex;
+
+            StringBuilder stringBuilder = new StringBuilder("");
+
+            float celsius = 0.0f, farenhiet = 0.0f, kelvin= 0.0f;
+
+            switch (selectedIndex)
+            {
+                case 0: // Celsius
+
+                     celsius = initialValue;
+                     farenhiet = (initialValue * (9/5)) + celsisFarenhietConversionRate;
+                     kelvin = initialValue + kelvinConversionRate;
+
+
+                    break;
+
+                case 1: // Farenhiet
+
+                    farenhiet = initialValue;
+                    celsius = (initialValue - celsisFarenhietConversionRate) * (5 / 9);
+                    kelvin = celsius + kelvinConversionRate;
+                    
+                    break;
+
+                case 2: // Kelvin
+
+                    kelvin = initialValue;
+                    celsius = initialValue - kelvinConversionRate;
+                    farenhiet = (celsius * (9 / 5)) + celsisFarenhietConversionRate;
+
+                    break;
+            }
+
+            stringBuilder.Append("Celsius " + celsius + "\r\n\r\n");
+            stringBuilder.Append("Farenhiet " + farenhiet + "\r\n\r\n");
+            stringBuilder.Append("Kelvin " + kelvin + "\r\n\r\n");
+            stringBuilder.Append("se esta calentando la cosa");
+
+            Temperature_Convertion_View.Text = stringBuilder.ToString();
+        }
+
+
+        private void convertMass()
+        {
+
+            string input = Mass_TextBox.Text;
+
+            float initialValue = (float)Convert.ToDouble(input);
+
+            int selectedIndex = Mass_ComboBox.SelectedIndex;
+
+            StringBuilder stringBuilder = new StringBuilder("");
+            float cm = 0.0f, m = 0.0f, ft = 0.0f;
+
+            switch (selectedIndex)
+            {
+                case 0: // cm cubico
+
+                    cm = initialValue;
+                    m = initialValue * cmConversionRateToMeter;
+                    ft = initialValue * cmConversionRateToFt;
+
+                    break;
+
+                case 1: // m cubico
+
+                    m = initialValue;
+                    cm = initialValue * mConversionRateToCentimeter;
+                    ft = initialValue * mConversionRateToFt;
+
+                    break;
+
+                case 2: // pies
+
+                    ft = initialValue;
+                    cm = ftConversionRateToCentimeter;
+                    m = ftConversionRateToMeter;
+
+                    break;
+            }
+
+            stringBuilder.Append("Centimetros cubicos " + cm + "\r\n\r\n");
+            stringBuilder.Append("Metros cubicos " + m + "\r\n\r\n");
+            stringBuilder.Append("Pies " + ft + "\r\n\r\n");
+            stringBuilder.Append("se esta alargando la cosa");
+
+            Mass_Convertion_View.Text = stringBuilder.ToString();
+        }
+
+        private void convertPressure()
+        {
+
+            string input = Pressure_TextBox.Text;
+
+            float initialValue = (float)Convert.ToDouble(input);
+
+            int selectedIndex = Pressure_ComboBox.SelectedIndex;
+
+            StringBuilder stringBuilder = new StringBuilder("");
+
+            float kgPerCmSquared = 0.0f, psi = 0.0f, kPa = 0.0f;
+
+            switch (selectedIndex)
+            {
+                case 0: // kg/cm^2
+
+                    kgPerCmSquared = initialValue;
+                    psi = initialValue * kgPerCmSquaredConversionRateToPsi;
+                    kPa = initialValue * kgPerCmSquaredConversionRateToKPa;
+
+                    break;
+
+                case 1: // PSI
+
+                    psi = initialValue;
+                    kgPerCmSquared = initialValue * psiConversionRateTokgPerCmSquared;
+                    kPa = initialValue * psiConversionRateToKPa;
+
+                    break;
+
+                case 2: // KPa
+
+                    kPa = initialValue;
+                    kgPerCmSquared = initialValue * kPaConversionRateToKgPerCmSquared;
+                    psi = initialValue * kPaConversionRateToPsi;
+
+                    break;
+            }
+
+            stringBuilder.Append("kg/cm^2 " + kgPerCmSquared + "\r\n\r\n");
+            stringBuilder.Append("PSI " + psi + "\r\n\r\n");
+            stringBuilder.Append("KPa " + kPa + "\r\n\r\n");
+            stringBuilder.Append("se esta apresionando la cosa");
+
+            Pressure_Convertion_View.Text = stringBuilder.ToString();
+        }
+
+        private void Convert_Btn_Click(object sender, EventArgs e)
+        {
+
+            getConversionRates();
+            convertTemperature();
+            convertMass();
+            convertPressure();
+
+        }
+
+        private void getConversionRates()
+        {
+            string pathToFile = "C:\\Users\\starw\\source\\repos\\Converter\\Conversions.txt";
+            string[] lines = File.ReadAllLines(pathToFile);
+
+            kelvinConversionRate = (float)Convert.ToDouble(lines[0]);
+            celsisFarenhietConversionRate = (float)Convert.ToDouble(lines[1]);
+
+            cmConversionRateToMeter = (float)Convert.ToDouble(lines[2]);
+            cmConversionRateToFt = (float)Convert.ToDouble(lines[3]);
+            mConversionRateToCentimeter = (float)Convert.ToDouble(lines[4]);
+            mConversionRateToFt = (float)Convert.ToDouble(lines[5]);
+            ftConversionRateToCentimeter = (float)Convert.ToDouble(lines[6]);
+            ftConversionRateToMeter = (float)Convert.ToDouble(lines[7]);
+
+            kgPerCmSquaredConversionRateToPsi = (float)Convert.ToDouble(lines[8]);
+            kgPerCmSquaredConversionRateToKPa = (float)Convert.ToDouble(lines[9]);
+            psiConversionRateTokgPerCmSquared = (float)Convert.ToDouble(lines[10]);
+            psiConversionRateToKPa = (float)Convert.ToDouble(lines[11]);
+            kPaConversionRateToKgPerCmSquared = (float)Convert.ToDouble(lines[12]);
+            kPaConversionRateToPsi = (float)Convert.ToDouble(lines[13]);
+
+        }
+    }
+}
